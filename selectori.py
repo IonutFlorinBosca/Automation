0dce8db6edadbc0c5c1ec30837490ccf59b91020
Ionut-Florin Bosca <98032042+IonutFlorinBosca@users.noreply.github.com>
2023-10-09 20:23:37 +0200

    Setup_Introducere_Selectori

---
+++ selectori.py
@@ -0,0 +1,7 @@
+# selenium 4
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
