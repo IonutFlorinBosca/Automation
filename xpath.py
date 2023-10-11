fff0f6b6c9446d5e46716e585531b222721dd0cf
Ionut-Florin Bosca <98032042+IonutFlorinBosca@users.noreply.github.com>
2023-10-11 10:24:45 +0200

    xpath completare formual de login si logare

--- xpath.py
+++ xpath.py
@@ -16,8 +16,23 @@ driver.get("https://www.saucedemo.com/v1/index.html")
 # timp de 3 secunde spre deosebire de time.sleep() care suspenda orice activitate
 driver.implicitly_wait(3)
 
-#accesez 'username' prin calea absoluta xpath
-username_field = driver.find_element(By.XPATH, "/html/body/div/div/div/div/form/input[@value='standard_user']")
+#accesez 'username' prin calea absoluta xpath cu identificatorul name
+username_field = driver.find_element(By.XPATH, "/html/body/div/div/div/div/form/input[@name='user-name']")
 username_field.send_keys("standard_user")
 
 time.sleep(2)
+
+#accesez 'password' prin calea relativa xpath cu identificatorul id
+password_field = driver.find_element(By.XPATH, "//input[@id='password']")
+password_field.send_keys("secret_sauce")
+
+time.sleep(2)
+
+#accesez butonul de submit al paginii prin identificatorul class si dau click pe el
+submit_button = driver.find_element(By.XPATH, "//input[@class='btn_action']")
+submit_button.click()
+
+time.sleep(2)
+
+#se inchide browser-ul
+driver.quit()
